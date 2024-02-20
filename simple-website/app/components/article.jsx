39cdf7d82d23fcb6e40cc680ace293a435f0a4c2
SonamWangyel <stevenrex229@gmail.com>
2024-02-20 15:58:00 +0600

    Made modification in website

--- simple-website/app/components/article.jsx
+++ simple-website/app/components/article.jsx
@@ -1,12 +1,12 @@
 export default function Article({ title, paragraphs }) {
 	return (
 		<>
-			<h1 className="text-3xl px-4 md:px-0 md:text-5xl text-center py-8">
+			<h1 className="text-3xl px-4 md:px-0 md:text-5xl text-center py-8 italic text-orange-500">
 				{title}
 			</h1>
 			{paragraphs.map((paragraph) => {
 				return (
-					<p className="pb-4 px-4 md:px-16 text-xl">
+					<p className="pb-4 px-4 md:px-16 text-xl text-lime-50">
 						{paragraph}
 					</p>
 				)
