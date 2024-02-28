ced90310c979c7814b5e8c0c3b1b5c6530a48e80
SonamWangyel <stevenrex229@gmail.com>
2024-02-28 15:29:38 +0600

    Created lyric finder

---
+++ app/searchlyric/lyrics.jsx
@@ -0,0 +1,48 @@
+// components/Lyrics.js
+import React, { useState } from 'react';
+
+const Lyrics = () => {
+  const [artist, setArtist] = useState('');
+  const [title, setTitle] = useState('');
+  const [lyrics, setLyrics] = useState('');
+
+  const fetchLyrics = async () => {
+    try {
+      const response = await fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`);
+      const data = await response.json();
+      setLyrics(data.lyrics);
+    } catch (error) {
+      console.error('Error fetching lyrics:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2 className='text-4xl '>Lyrics Finder</h2>
+      <input className='text-black border-4 border-red-300 flex items-center justify-center'
+        type="text"
+        placeholder="Artist"
+        value={artist}
+        onChange={(e) => setArtist(e.target.value)}
+      />
+      <input className='text-black border-4 border-red-300 flex items-center justify-center'
+        type="text"
+        placeholder="Song Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button  
+        className="border-4 border-white "
+        onClick={fetchLyrics}
+        >
+        Get Lyrics
+      </button>
+      <div>
+        <h3>Lyrics</h3>
+        <pre className="text-amber-400 text-center">{lyrics}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default Lyrics;
---
+++ app/searchlyric/page.js
@@ -0,0 +1,13 @@
+"use client"
+import Lyrics from './lyrics';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-6xl '>Welcome to My Lyrics Website</h1>
+      <Lyrics />
+    </div>
+  );
+};
+
+export default Home;
