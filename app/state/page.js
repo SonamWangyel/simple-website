2afa1d466bce30c271574da168a1621fd9f5590c
SonamWangyel <stevenrex229@gmail.com>
2024-02-28 14:15:49 +0600

    Practice state and effect

---
+++ app/state/page.js
@@ -0,0 +1,25 @@
+"use client"
+
+import { useState } from "react"
+
+export default function State() {
+	const [word, setWord] = useState("Hello")
+	// var word = "Hello" // Using a regular variable like this won't work for dynamic content
+	return (
+		<div>
+			<p className="text-4xl">{word}</p>
+			<input
+				// onChange={(event) => {
+				// 	console.log("BEFORE: " + word)
+				// 	word = event.target.value
+				// 	console.log("AFTER: " + word)
+				// }}
+				onChange={(event) => {
+					console.log("BEFORE: " + word)
+					setWord(event.target.value)
+					console.log("AFTER: " + word)
+				}}
+			/>
+		</div>
+	)
+}   
